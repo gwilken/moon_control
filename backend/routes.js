e48fd62a27cb29253d15db251e004d1a54850306
greg <greg@gwilken.com>
2019-02-14 20:30:29 -0800

    Testing auth

--- backend/routes.js
+++ backend/routes.js
@@ -6,56 +6,59 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
 
 router.get('/key/:key/:start?/:end?', async (req, res) => {  
-    console.log(req.params.key)
-    let data = await redisUtils.getHashsFromSet(req.params.key, req.params.start, req.params.end)
-    console.log(data)
-    res.json(data)
+  console.log(req.params.key)
+  let data = await redisUtils.getHashsFromSet(req.params.key, req.params.start, req.params.end)
+  console.log(data)
+  res.json(data)
 })
 
 router.get('/window/:key/:window', async (req, res) => {  
-    let data = await redisUtils.getHashsFromSet(req.params.key, req.params.window * -1, -1)
-    res.json(data)
+  let data = await redisUtils.getHashsFromSet(req.params.key, req.params.window * -1, -1)
+  res.json(data)
 })
 
 router.get('/allkeys', async (req, res) => {  
-    let data = await redisUtils.getHashsFromSet(req.params.key, req.params.start, req.params.end)
-    res.json(data)
+  let data = await redisUtils.getHashsFromSet(req.params.key, req.params.start, req.params.end)
+  res.json(data)
 })
 
 router.post('/key', jsonParser, (req, res) => {
-    console.log(req.body)
-    res.send('OK')
-   // redisUtils.addHashUpdateSet()
+  console.log(req.body)
+  res.send('OK')
+  // redisUtils.addHashUpdateSet()
 })
 
 router.post('/update',
-    passport.authenticate('basic', { session: false }),
-    jsonParser,
-    (req, res) => {
-        // If this function gets called, authentication was successful.
-        // `req.user` contains the authenticated user.
-        //res.redirect('/users/' + req.user.username);
-        console.log('>>>>>>AUTHENTICATED>>>>')
-        //console.log(req.body)
+  passport.authenticate('basic', { session: false }),
+  jsonParser,
+  (req, res) => {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      //res.redirect('/users/' + req.user.username);
+      console.log('>>>>>>AUTHENTICATED>>>>')
+      //console.log(req.body)
 
-        const data = req.body;
+      const data = req.body;
 
-        Object.keys(data).map(key => {
-            if(data[key].set && data[key].hashkey) {
-                let arr = Object.entries(data[key])
-                redisUtils.redis.hmset(data[key].hashkey, arr.flat())
-                console.log('hash added:', data[key].hashkey)
-                
-                redisUtils.redis.zadd(data[key].set, parseFloat(data[key].timestamp), data[key].hashkey)
-                console.log('set added:', data[key].set)
+      Object.keys(data).map(key => {
+        if (data[key].set && data[key].hashkey) {
+          let arr = Object.entries(data[key])
+          redisUtils.redis.hmset(data[key].hashkey, arr.flat())
+          console.log('hash added:', data[key].hashkey)
+          
+          redisUtils.redis.zadd(data[key].set, parseFloat(data[key].timestamp), data[key].hashkey)
+          console.log('set added:', data[key].set)
 
-                //r.zadd(set + '-set', hashkey, timestamp )
-            } 
-        })
+        } else if (data[key].type) {
+          let arr = Object.entries(data[key])
+          redisUtils.redis.hmset(data[key].type, arr.flat())
+          console.log('hash added:', data[key].type)
+        }
+      })
 
 
-        res.end()
-    })
+      res.end()
+  })
 
 // router.post('/', jsonParser, (req, res) => {
 //     console.log(req.body)
